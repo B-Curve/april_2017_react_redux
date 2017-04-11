a5e8bd02990b1010bf737aa454f7230837b2b414
Brandon Kervin <brandonkervin@gmail.com>
2017-04-11 15:17:31 -0500

    added desktop page

---
+++ public/layouts/desktopLayout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class DesktopLayout extends React.Component{
+  render(){
+    return(
+      <h1>Hello from React! Desktop version not functioning yet.</h1>
+    );
+  }
+}
